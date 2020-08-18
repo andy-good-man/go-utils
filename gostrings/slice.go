e0cd5ba05d9911ca3382bbff3f3c7463c9f3ab62
andy-good-man <andy21chu@gmail.com>
2020-08-18 13:06:55 +0800

    updated gostrings

--- gostrings/slice.go
+++ gostrings/slice.go
@@ -4,6 +4,8 @@ import (
 	"strings"
 )
 
+// {in}.{exist}
+
 // SliceIn 某值item是否在数组中
 func SliceIn(slice []string, item string) bool {
 	for _, val := range slice {
@@ -34,10 +36,68 @@ func SliceInTrimSpace(slice []string, item string) bool {
 	return false
 }
 
+// {reverse}.{sort}
+
 // SliceReverse 将数组元素顺序反向排序
 func SliceReverse(slice []string) []string {
 	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
 		slice[i], slice[j] = slice[j], slice[i]
 	}
 	return slice
+}
+
+// {union}
+
+// SliceUnion 求并集 slice1+slice2
+func SliceUnion(slice1, slice2 []string) []string {
+	m := make(map[string]int) // 统计数组中某个元素的出现次数
+
+	for _, v := range slice1 {
+		m[v]++
+	}
+	for _, v := range slice2 {
+		times, _ := m[v]
+		if times == 0 {
+			slice1 = append(slice1, v)
+		}
+	}
+
+	return slice1
+}
+
+// SliceIntersect 求交集
+func SliceIntersect(slice1, slice2 []string) []string {
+	m := make(map[string]int)
+	nn := make([]string, 0)
+
+	for _, v := range slice1 {
+		m[v]++
+	}
+	for _, v := range slice2 {
+		times, _ := m[v]
+		if times == 1 {
+			nn = append(nn, v)
+		}
+	}
+
+	return nn
+}
+
+// SliceDifference 求差集{A-(A和B交集)} slice1-slice2
+func SliceDifference(slice1, slice2 []string) []string {
+	m := make(map[string]int)
+	nn := make([]string, 0)
+
+	inter := SliceIntersect(slice1, slice2)
+	for _, v := range inter {
+		m[v]++
+	}
+	for _, value := range slice1 {
+		times, _ := m[value]
+		if times == 0 {
+			nn = append(nn, value)
+		}
+	}
+
+	return nn
 }
--- gostrings/slice_test.go
+++ gostrings/slice_test.go
@@ -22,3 +22,27 @@ func TestSliceReverse(t *testing.T) {
 	reverse := SliceReverse(slice)
 	fmt.Println("reverse  :", reverse)
 }
+
+func TestSliceUnion(t *testing.T) {
+	slice1 := []string{"1", "2", "33", "44"}
+	slice2 := []string{"你好", "VIP", "咖喱鸡块和刻入啊", "44", "44", "55", "66", "44", "77"}
+
+	result := SliceUnion(slice1, slice2)
+	fmt.Println("result  :", result)
+}
+
+func TestSliceIntersect(t *testing.T) {
+	slice1 := []string{"1", "2", "33", "44"}
+	slice2 := []string{"你好", "VIP", "咖喱鸡块和刻入啊", "44", "44", "55", "66", "44", "77"}
+
+	result := SliceIntersect(slice1, slice2)
+	fmt.Println("result  :", result)
+}
+
+func TestDifference(t *testing.T) {
+	slice1 := []string{"1", "2", "33", "44"}
+	slice2 := []string{"你好", "VIP", "咖喱鸡块和刻入啊", "44", "44", "55", "66", "44", "77"}
+
+	result := SliceDifference(slice1, slice2)
+	fmt.Println("result  :", result)
+}
