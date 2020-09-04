76802fd015049f8d3b5fff1cc5ef5bd9dcff65f5
andy-good-man <andy21chu@gmail.com>
2020-09-04 20:17:14 +0800

    zhoumo

--- gostrings/slice_test.go
+++ gostrings/slice_test.go
@@ -54,3 +54,23 @@ func TestDifference(t *testing.T) {
 
 	fmt.Println("result  :", result)
 }
+
+type Example struct {
+	Content string
+}
+
+func (e *Example) Clone() *Example {
+	res := *e
+	return &res
+}
+
+func TestClone(t *testing.T) {
+	r1 := new(Example)
+	r1.Content = "this is example 1"
+	r2 := r1.Clone()
+	r2.Content = "this is example 2"
+
+	fmt.Println(r1)
+	fmt.Println(r2)
+
+}
